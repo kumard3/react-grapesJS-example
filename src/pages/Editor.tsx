ee187db8c66d4ed481ee8ff315460028370c85dc
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-30 14:08:51 +0530

    basic editor done

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,18 +1,24 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 //@ts-ignore
-import grapesjs from 'grapesjs'
+import grapesjs from "grapesjs";
+//@ts-ignore
+import gjsPresetWebPage from "grapesjs-preset-webpage";
+
 export default function Editor() {
-
-    const [editor ,setEditor] = useState(null);
-useEffect(()=>{
-const editor = grapesjs.init({
-    container:"#editor",
-})
+  const [editor, setEditor] = useState(null);
+  useEffect(() => {
+    const editor = grapesjs.init({
+      container: "#editor",
+      plugins: [gjsPresetWebPage],
+      pluginsOpts: {
+        gjsPresetWebPage: {},
+      },
+    });
     setEditor(editor);
-
-},[])
+  }, []);
   return (
-  <div>
-<div id="editor"></div>
-  </div>)
+    <div>
+      <div id="editor"></div>
+    </div>
+  );
 }
